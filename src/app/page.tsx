3d033c6ed90040164cbcd35a61dcf83af8622ece
Prajhub <pzlmagar@gmail.com>
2024-07-04 13:08:21 +0545

    setup db with turso and drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import { db } from "@/db";
+import { postsTable, usersTable } from "@/db/schema";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const post = await db.query.postsTable.findMany()
+
   return (
-    <div>hi</div>
+    <div>hi
+
+
+      <form action={async () => {
+        'use server'
+        await db.insert(usersTable).values({
+          id: 1,
+          age: 21,
+          email: "text@example.com",
+          name: "syco"
+        })
+
+        await db.insert(postsTable).values({
+          title: "Was gang",
+          content: "yolo",
+          userId: 1
+        })
+      }}><button>submit</button></form>
+
+      {post.map(post => (
+        <div>{post.title}</div>
+      ))}
+    </div>
   );
 }
